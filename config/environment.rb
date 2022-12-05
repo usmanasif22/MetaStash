c93a33a585d31953976334630daab08394316104
USMAN ASIF <sp19-bcs-050@cuilahore.edu.pk>
2022-12-05 17:44:21 +0500

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ require_relative "application"
 Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-  :password => Rails.application.credentials.sendgrid[:api_key], # This is the secret sendgrid API key which was issued during API key creation
-  :domain => 'meta-stash.herokuapp.com/admin/dashboard',
+  :password => Rails.application.credentials.dig(:sendgrid, :api_key), # This is the secret sendgrid API key which was issued during API key creation
+  :domain => 'meta-stash.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
