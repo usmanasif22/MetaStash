ba4d3d4b127e56cecc8e3ee3444155b68c40adfe
USMAN ASIF <sp19-bcs-050@cuilahore.edu.pk>
2022-12-05 15:57:38 +0500

    Update dashboard.rb

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,9 +3,9 @@ ActiveAdmin.register_page "Dashboard" do
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
   content :title => proc{ I18n.t("active_admin.dashboard") } do
 
-    panel "Users" do
-      render 'admin/import/chart'
-    end
+    # panel "Users" do
+    #   render 'admin/import/chart'
+    # end
   
 
   end
